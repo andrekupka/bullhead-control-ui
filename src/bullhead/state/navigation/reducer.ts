5ce1c599e8746339394ae035567519799b5d5150
Andre Kupka <me-contact@andrekupka.de>
2019-09-02 20:16:47 +0200

    add not found page

--- src/bullhead/state/navigation/reducer.ts
+++ src/bullhead/state/navigation/reducer.ts
@@ -6,7 +6,7 @@ interface NavigationState extends NavigationAware {
 
 const INITIAL_STATE: NavigationState = {
     navigationWidth: 240,
-    isNavigationOpen: false
+    isNavigationOpen: true
 };
 
 export const navigationReducer = (state: NavigationState = INITIAL_STATE, action: NavigationActionTypes): NavigationState => {
