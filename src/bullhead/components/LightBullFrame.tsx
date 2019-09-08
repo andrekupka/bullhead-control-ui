fb185fb075278b4f117f3b441181897abefac726
Andre Kupka <me-contact@andrekupka.de>
2019-09-08 15:02:32 +0200

    fix layout

--- src/bullhead/components/LightBullFrame.tsx
+++ src/bullhead/components/LightBullFrame.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import {LightBullContentContainer} from './MainContentContainer';
 import {NavigationBar} from './navigation/NavigationBar';
+import {makeStyles} from "@material-ui/core";
+
+const useStyles = makeStyles({
+    frame: {
+        display: 'flex'
+    }
+});
 
 export const LightBullFrame = () => {
+    const classes = useStyles();
+
     return (
-      <div>
-          <NavigationBar/>
-          <LightBullContentContainer/>
-      </div>
+        <div className={classes.frame}>
+            <NavigationBar/>
+            <LightBullContentContainer/>
+        </div>
     );
 };
