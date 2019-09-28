dfab825589fdc69a984fe3704d8e1fa9a6378200
Andre Kupka <me-github@andrekupka.de>
2019-09-28 22:02:52 +0200

    fix proxy config and make visual name editable

--- src/bullhead/components/visual/VisualView.tsx
+++ src/bullhead/components/visual/VisualView.tsx
@@ -5,17 +5,19 @@ import {selectShow} from '../../state/model/shows/selectors';
 import {selectVisual} from '../../state/model/visuals/selectors';
 import {connect} from 'react-redux';
 import React from 'react';
+import {VisualName} from './VisualName';
 
 interface Props {
     show: Show;
     visual: Visual;
 }
 
-const PureVisualView = (props: Props) => {
-    return <div>
-        <p>Show {props.show.name}</p>
-        <p>Visual {props.visual.name}</p>
-    </div>;
+const PureVisualView = ({show, visual}: Props) => {
+    return <>
+        <VisualName visual={visual}/>
+        <p>Show {show.name}</p>
+        <p>Visual {visual.name}</p>
+    </>;
 };
 
 interface WrapperProps {
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,16 +1,11 @@
 var proxy = require('http-proxy-middleware');
 
 module.exports = function (app) {
-    app.use(proxy('/api',{
+    app.use('/api', proxy({
         target: 'http://localhost:8080',
         changeOrigin: true
     }));
-    app.use(proxy('/ws',{
-        target: 'ws://localhost:8080',
-        changeOrigin: true,
-        ws: true
-    }));
-    app.use(proxy('/sockjs-node',{
+    app.use('/ws', proxy({
         target: 'ws://localhost:8080',
         changeOrigin: true,
         ws: true
